a98b6c04d692e5620366da32ff16f888561faa1a
newmorin2 <newton.mwangi@student.moringaschool.com>
2026-07-30 14:07:13 +0300

    configuration so backend & frontend works, done authentication & created models

--- api/views.py
+++ api/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import generics
+
+from django.contrib.auth.models import User
+
+from .serializers import RegisterSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+
+@api_view(["GET"])
+def current_user(request):
+
+    return Response({
+        "username":request.user.username,
+        "email":request.user.email
+
+    })
