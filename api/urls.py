2a408cbec40a6366af7b13241f58863fa0476c19
Ric Musyoki <ric.musyoki@student.moringaschool.com>
2026-07-31 11:54:14 +0300

    Fixed isuues to do with backend

--- api/urls.py
+++ api/urls.py
@@ -4,60 +4,42 @@ from .views import (
     current_user,
     DepartmentListCreateView,
     DepartmentDetailView,
-)
-
-from .views import (
     AppointmentListCreateView,
     DepartmentListView,
     DoctorListView,
     ProblemReportListCreateView,
-    RegisterView,
-    current_user,
     hospital_info,
 )
 
-urlpatterns=[
+urlpatterns = [
     path(
         "",
-        hospital_info
+        hospital_info,
     ),
-
-urlpatterns = [
     path(
         "signup/",
         RegisterView.as_view(),
     ),
-
     path(
         "me/",
-        current_user
+        current_user,
     ),
     path(
         "departments/",
-        DepartmentListView.as_view()
+        DepartmentListView.as_view(),
     ),
     path(
         "doctors/",
-        DoctorListView.as_view()
+        DoctorListView.as_view(),
     ),
     path(
         "appointments/",
-        AppointmentListCreateView.as_view()
+        AppointmentListCreateView.as_view(),
     ),
     path(
         "problems/",
-        ProblemReportListCreateView.as_view()
-    )
-
-]
-        current_user,
+        ProblemReportListCreateView.as_view(),
     ),
-
-    path(
-        "departments/",
-        DepartmentListCreateView.as_view(),
-    ),
-
     path(
         "departments/<int:pk>/",
         DepartmentDetailView.as_view(),
